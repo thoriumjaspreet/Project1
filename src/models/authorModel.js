3a0e303f05ce88c33777fd184a926a4bcc1c9d28
saiprasad <rsaiprasad4@gmail.com>
2022-04-26 19:20:53 +0530

    done

---
+++ src/models/authorModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const newSchema = new mongoose.Schema(
+  {
+    fname: String,
+    lname: String,
+    title: {
+      type: String,
+      required: true,
+      enum: ["Mr", "Mrs", "Miss"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("Author", newSchema);
