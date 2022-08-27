acfb94a72ddc1c3c91ba9c39ef8231e65f25744e
saiprasad <rsaiprasad4@gmail.com>
2022-08-27 12:40:13 +0530

    Refactored

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 app.use("/", route);
 
 const port = process.env.PORT || 3000
+
 app.listen(process.env.PORT || 3000, function () {
   console.log(`Express app running on ${port}...`)
 });
